4685dcf95194f6269a7223da9ed76a18ee70c5c0
Hehoku <hehoku@outlook.com>
2022-05-15 10:51:22 +0800

    feat: support draw progress bar on image canvas

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   const [target, setTarget] = useState(10000)
   const [current, setCurrent] = useState(2000)
+  const [progress, setProgress] = useState(target && current / target)
   const [selectedImage, setSelectedImage] = useState(null)
 
   const hiddenFileInput = useRef(null)
@@ -36,8 +37,11 @@ export default function Home() {
     return doneEmoji + inProgressEmoji
   }
 
+  // DONE: draw an image on canvas, and add circle process decoration
+  // DONE: add two circular ring, one as background, the other as progress
   useEffect(() => {
     if (canvasRef.current && selectedImage) {
+      console.log('drawing image')
       const context = canvasRef.current.getContext('2d')
       const w = context.canvas.width
       const h = context.canvas.height
@@ -45,29 +49,60 @@ export default function Home() {
       const centerY = h / 2
       const radius = Math.min(w, h) / 2
 
+      context.clearRect(0, 0, w, h)
+
+      // draw background
       context.beginPath()
       context.arc(centerX, centerY, radius, 0, 2 * Math.PI)
-      context.fillStyle = 'green'
-      context.clip()
+      context.fillStyle = 'black'
+      context.fill()
+      // context.clip()
 
+      // circular ring for progress
+      context.beginPath()
+      context.arc(
+        centerX,
+        centerY,
+        radius - 10,
+        -Math.PI * 0.5,
+        2 * Math.PI * progress - Math.PI * 0.5,
+        false
+      )
+      context.lineWidth = 20
+      context.lineCap = 'round'
+      var gradient = context.createLinearGradient(0, 0, w / 2, h / 2)
+      gradient.addColorStop(0, 'yellow')
+      gradient.addColorStop(0.25, 'cyan')
+      gradient.addColorStop(0.5, 'orange')
+      gradient.addColorStop(0.75, 'red')
+      gradient.addColorStop(1, 'green')
+      context.strokeStyle = gradient
+      context.stroke()
+
+      // draw image
+      context.beginPath()
+      context.arc(centerX, centerY, radius - 10, 0, 2 * Math.PI)
       const image = new window.Image()
       console.log(selectedImage)
       image.src = URL.createObjectURL(selectedImage)
       image.onload = () => {
         context.drawImage(image, 0, 0, w, h)
       }
-
-      context.beginPath()
-      context.arc(centerX, centerY, radius - 20, 0, 2 * Math.PI)
       context.clip()
     }
-  }, [selectedImage])
+  }, [progress, selectedImage])
 
-  // DONE: draw an image on canvas, and add circle process decoration
+  useEffect(() => {
+    setProgress(target && current / target)
+  }, [target, current])
+
+  // TODO: current user should set the target and current, then upload an image
+  // other can't generate the progress
+
   return (
     <div className="flex h-screen flex-row items-center justify-center">
       <div className="flex flex-col items-center justify-center">
-        <div className="h-32 w-32 rounded-full bg-gray-500">
+        <div className="h-32 w-32 bg-gray-500">
           {' '}
           {selectedImage && (
             <div>
@@ -115,7 +150,7 @@ export default function Home() {
         </label>
         <p className="mt-4 text-2xl font-bold">
           {convertPercentageToEmoji(current / target)}
-          {target && ((current / target) * 100).toFixed(2)}%
+          {(progress * 100).toFixed(2)}%
         </p>
       </div>
     </div>
